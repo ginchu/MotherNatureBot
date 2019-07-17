de871696ea0fbfb1cd88154d354cb9ea724d0e1f
Gina <ginakchun@gmail.com>
2019-07-17 17:10:36 -0400

    os.environ

--- my_twitter_bot.py
+++ my_twitter_bot.py
@@ -3,7 +3,11 @@ import time
 from keys import *
 print("This is my twitter bot")
 
-
+from os import environ
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
